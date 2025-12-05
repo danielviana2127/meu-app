c6fdc8f47011011ff3b024fbbdbadae3e7e6b76f
daniel <dvianaa@yahoo.com.br>
2025-12-05 10:33:08 -0300

    Aplicação Python + PostgresSQL com Docker

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import psycopg2
+
+app = Flask(__name__)
+
+def get_connection():
+    return psycopg2.connect(
+        host="postgres-db",
+        database="meubanco",
+        user="meuuser",
+        password="minhasenha"
+    )
+
+@app.route("/")
+def index():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        result = cur.fetchone()
+        return f"Conectado ao PostgreSQL: {result}"
+    except Exception as e:
+        return f"Erro ao conectar: {e}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
